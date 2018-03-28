d282c812f8de64571b49bb1c30eb016376cbe723
Godfred <gny2@njit.edu>
2018-03-28 01:40:26 -0400

    Create a database seed to generate 50 cars. Also enhance CarFactory.

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -2,11 +2,30 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Car::class, function (Faker $faker) {
+$factory->define(/**
+ * @param Faker $faker
+ * @return array
+ */
+    App\Car::class, function (Faker $faker) {
+
+        /* Fake a Make and then fake Model accordingly
+            - consider faking a randomElement where the element is a pair of info
+        */
+        $fakedMake = $faker->randomElement(['Ford','Honda','Toyota']);
+        if($fakedMake == 'Ford'){
+            $fakedModel = $faker->randomElement(['Edge','Escape','Expedition', 'Explorer']);
+        } elseif ($fakedMake == 'Honda'){
+            $fakedModel = $faker->randomElement(['Accord Coupe','Accord Hybrid','Accord Sedan',
+                'Civic Coupe','Civic Hatchback','Civic Sedan',
+                'CR-V','Fit','Odyssey'
+                ]);
+        } else {
+            $fakedModel = $faker->randomElement(['Avalon','Camry','Highlander', 'Tundra']);
+        }
     return [
         //
-        'make' => $faker->randomElement('Ford','Honda','Toyota'),
-        'model' => $faker->domainWord,
-        'year' => $faker->numberBetween(1989, 2019), // secret
+        'make' => $fakedMake,
+        'model' => $fakedModel,
+        'year' => $faker->numberBetween(1989, 2019),
     ];
 });
