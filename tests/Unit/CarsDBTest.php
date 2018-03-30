d85fc26ad806b263fb82681ad1e9f6fd3c43994b
Godfred <gny2@njit.edu>
2018-03-30 18:16:21 -0400

    Added unit test to test updating a car year.

--- tests/Unit/CarsDBTest.php
+++ tests/Unit/CarsDBTest.php
@@ -21,4 +21,29 @@ class CarsDBTest extends TestCase
         $NewCar = factory(Car::class)->create();
         $this->assertDatabaseHas('cars',['id'=>$NewCar->id]);
     }
+
+    /**
+     * Find MaxID with Year not equal to the target
+     * Record the year; Change it; Take copy of the changed year
+     * Use assertion but also echo the copies for visual inspection
+     *
+     * @return void
+     */
+    public function testCarUpdateYear()
+    {
+        $tgtYear = 2000;
+        $tgtCarID = DB::table('cars')
+                        ->where('year', '<>', $tgtYear)
+                        ->max('id');
+
+        $tgtCarYearB4 = DB::table('cars')->where('id',$tgtCarID)->max('year');
+        DB::table('cars')->where('id',$tgtCarID)->update(['year'=>$tgtYear]);
+        $tgtCarYearAfter = DB::table('cars')->where('id',$tgtCarID)->max('year');
+
+        echo PHP_EOL . 'testCarUpdate: target carID = '. (string)$tgtCarID
+            . '; Year before = ' . (string)$tgtCarYearB4
+            . '; AND Year after = ' . (string)$tgtCarYearAfter . PHP_EOL;
+
+        $this->assertDatabaseHas('cars',['id'=>$tgtCarID, 'year'=>$tgtYear]);
+    }
 }
