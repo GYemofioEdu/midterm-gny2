4c7a3889f1b197b94d19599e1a23858a5f13540c
Godfred <gny2@njit.edu>
2018-03-30 02:36:23 -0400

    Added a PagesTest class, and added 2 functions - test for Register Page, and test for Login Page. Deleted the RegisterTest unit test class (as replaced by function)...

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -1,21 +1,27 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class RegisterTest extends TestCase
+class PagesTest extends TestCase
 {
     /**
-     * A basic test example.
+     * This class is dedicated to pages/uri (get) tests
      *
      * @return void
      */
     public function testRegisterPage()
     {
         $response = $this->get('/register');
+        $response->assertStatus(200);
+    }
+
+    public function testLoginPage()
+    {
+        $response = $this->get('/login');
         $response->assertStatus(200);
     }
 }
